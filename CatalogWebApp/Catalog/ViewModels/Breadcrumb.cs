7eadda932c478af2a6b65829804d2debf533029d
vkrypulevich <Volha_Krypulevich@epam.com>
2021-07-07 23:40:24 +0300

    Task 2. ViewComponents / PartialViews

---
+++ CatalogWebApp/Catalog/ViewModels/Breadcrumb.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Catalog.ViewModels
+{
+    public class Breadcrumb
+    {
+        public Breadcrumb(string controller, string action, string title, object id)
+            : this(controller, action, title)
+        {
+            Id = id;
+        }
+
+        public Breadcrumb(string controller, string action, string title)
+        {
+            Controller = controller;
+            Action = action;
+
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                Title = Regex.Replace(
+                    CultureInfo.CurrentCulture.TextInfo.ToTitleCase(
+                        string.Equals(action, "Index", StringComparison.OrdinalIgnoreCase) ? controller : action), "[a-z][A-Z]", m => m.Value[0] + " " + char.ToLower(m.Value[1]));
+            }
+            else
+            {
+                Title = title;
+            }
+        }
+
+        public string Controller { get; set; }
+
+        public string Action { get; set; }
+
+        public object Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ CatalogWebApp/Catalog/ViewModels/BreadcrumpViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.ViewModels
+{
+    public class BreadcrumbViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Microsoft.AspNetCore.Mvc.Rendering.ViewContext viewContext)
+        {
+            var routeValues = viewContext?.RouteData?.Values;
+
+            string controller = routeValues["Controller"]?.ToString();
+            string action = routeValues["Action"]?.ToString();
+            object id = routeValues["Id"];
+            string title = viewContext.ViewData["Title"]?.ToString(); ;
+            var breadcrumbs = new List<Breadcrumb>();
+
+            breadcrumbs.Add(new Breadcrumb("Home", "Index", "Home"));
+            breadcrumbs.Add(new Breadcrumb(controller, action, title, id));
+            if (!string.Equals(action, "Index", StringComparison.InvariantCultureIgnoreCase))
+            {
+                breadcrumbs.Insert(1, new Breadcrumb(controller, "Index", controller));
+            }
+
+            return View("BreadcrumbViewComponent", breadcrumbs);
+        }
+
+    }
+}
