639d837b7678de4f2f8e0bc9b125d8db4ee6892d
vkrypulevich <Volha_Krypulevich@epam.com>
2021-07-09 18:07:20 +0300

    Task 3. Html Helpers / Html Tags (Tag Helpers)

---
+++ CatalogWebApp/Catalog/HtmlHelpers/HtmlHelpers.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Catalog.HtmlHelpers
+{
+    public static class HtmlHelpers
+    {
+        private const string IMAGE_LINK = "<a href=\"Image/{0}\">{1}</a>";
+
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper htmlHelper, int imageId, string linkText)
+            => new HtmlString(string.Format(IMAGE_LINK, imageId, linkText));
+    }
+}
