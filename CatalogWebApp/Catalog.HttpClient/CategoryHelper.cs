04ffe094c00173c05cdff96d0b7dadb48813cadf
vkrypulevich <Volha_Krypulevich@epam.com>
2021-07-25 20:27:43 +0300

    Task 2. Simple REST clients, Console .Net [Core]

---
+++ CatalogWebApp/Catalog.HttpClient/CategoryHelper.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Catalog.HttpClient
+{
+	public static class CategoryHelpers
+	{
+		public static void ShowCategory(List<Category> categories)
+		{
+			foreach (var category in categories)
+			{
+				Console.WriteLine($"Name: {category.CategoryName}, Description: {category.Description}");
+			}
+		}
+
+		public static async Task<List<Category>> GetCategoriesAsync(System.Net.Http.HttpClient client, string path)
+		{
+			List<Category> categories = new List<Category>();
+			HttpResponseMessage response = await client.GetAsync(path);
+			if (response.IsSuccessStatusCode)
+			{
+				categories = await response.Content.ReadAsAsync<List<Category>>();
+			}
+
+			return categories;
+		}
+	}
+}
---
+++ CatalogWebApp/Catalog.HttpClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Catalog.HttpClient
+{
+	class Program
+	{
+		static System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+
+		static void Main()
+		{
+			RunAsync().GetAwaiter().GetResult();
+		}
+
+		static async Task RunAsync()
+		{
+			client.BaseAddress = new Uri("http://localhost:15457/api/");
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("application/json"));
+
+			try
+			{
+				var products = await ProductHelper.GetProductsAsync(client, "products");
+				ProductHelper.ShowProduct(products);
+
+				var categories = await CategoryHelpers.GetCategoriesAsync(client, "categories");
+				CategoryHelpers.ShowCategory(categories);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
